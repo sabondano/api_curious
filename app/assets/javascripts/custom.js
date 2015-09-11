52db9ed0a4c5aee215fe2b04613c8a6bf42a8a9f
Sebastian Abondano <sabondano1@gmail.com>
2015-09-11 01:40:16 -0600

    Move js to fadeToggle reply form into dashboard

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,8 +2,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
-
-// Make reply to tweet form appear
-$(".glyphicon-share-alt").on("click", function() {
-  $(this).parents(".tweet").next(".reply-form").fadeToggle();
-});
